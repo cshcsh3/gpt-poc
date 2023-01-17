57528e2655d9ac3588d473b21eee61431b7ade20
cshcsh3 <thecsh3@gmail.com>
2023-01-17 15:31:16 +0800

    Write to csv

--- gpt.py
+++ gpt.py
@@ -13,21 +13,26 @@ def query_gpt(topic):
   query = index.query("Given the following video information, on a scale of 1-10, how confident are you that the video talks about {}?".format(topic))
   return re.sub(r"\n", "", query.response)
 
-with open('videos.csv', 'r') as videos:
-  f = open('ratings.txt', 'w')
+with open('top.csv', 'r') as videos:
+  f = open('ratings.csv', 'w')
+  headers = ['url','keyword','manual_rating','gpt_rating','partnership_id','total_referrals']
+  writer = csv.writer(f)
+  writer.writerow(headers)
+
   csv_reader = csv.reader(videos, delimiter=',')
   line_count = 0
   for video_row in csv_reader:
-    if line_count > 0:      
+    if line_count > 0:
       url = video_row[0]
-      topic = video_row[1]
+      keyword = video_row[1]
+      manual_rating = video_row[2]
+      partnership_id = video_row[3]
+      total_referrals = video_row[4]
 
       yt_info.generate(url)
-      response = query_gpt(topic)
+      response = query_gpt(keyword)
+      writer.writerow([url, keyword, manual_rating, response, partnership_id, total_referrals])
       
-      # TODO Write to a csv file
-      f.write(f'{url} - Topic: {topic}, Manual rating: {video_row[4]}, GPT rating: {response}\n')
-
     line_count += 1
     time.sleep(5)
   
